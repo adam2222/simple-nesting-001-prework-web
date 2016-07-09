f251d6700afc16cc996cead5d44108312e3c9f5e
adam2222 <adam222@comcast.net>
2016-07-08 21:29:29 -0400

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,6 @@
 
+require 'byebug'
+
 def hopper
 	programmer_hash = 
  		{
@@ -16,7 +18,7 @@ def hopper
         }
      }
 
-
+    programmer_hash[:grace_hopper] 
 end
 
 def alan_kay_is_known_for
@@ -37,6 +39,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,6 +86,10 @@ def adding_matz
      }
 
     
+    programmer_hash[:yukihiro_matsumoto] = {}
+    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+    programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP, C"]
+    programmer_hash
 end
 
 def changing_alan
@@ -102,6 +111,8 @@ def changing_alan
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
 end
 
@@ -122,7 +133,9 @@ def adding_to_dennis
         }
      }
 
-     
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  
+  programmer_hash
 end
 
 
